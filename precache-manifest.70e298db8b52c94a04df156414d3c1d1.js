d29c8761cfc875226b49077cfd4c689a98a42b9d
Nilam Trivedi <51967991+NTink@users.noreply.github.com>
2019-06-19 06:58:08 -0400

    Add files via upload
    I have created a customer-facing application that accepts a City as a parameter. The application will display the following information about each restaurant that delivers to that City, in an aesthetically pleasing way:
    •	Name
    •	Address
    •	Price
    I have also added an extra feature of a drop-down list for more restaurant options

---
+++ precache-manifest.70e298db8b52c94a04df156414d3c1d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64e1e72b2493b33d495031695619c5fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c4997ee5edfa277392f",
+    "url": "/static/css/main.8c57e09e.chunk.css"
+  },
+  {
+    "revision": "36c6d2ff2f153374c56b",
+    "url": "/static/js/2.4834304d.chunk.js"
+  },
+  {
+    "revision": "0c4997ee5edfa277392f",
+    "url": "/static/js/main.96485b24.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ precache-manifest.bdab57d46c61085dab0652132e903f0a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccefb81b47537b76ceb3334590cd8f00",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4ff9ce7351d115e847e7",
+    "url": "./static/css/main.8c57e09e.chunk.css"
+  },
+  {
+    "revision": "36c6d2ff2f153374c56b",
+    "url": "./static/js/2.4834304d.chunk.js"
+  },
+  {
+    "revision": "4ff9ce7351d115e847e7",
+    "url": "./static/js/main.f725881b.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
